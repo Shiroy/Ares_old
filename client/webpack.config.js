10f72f6f824d4944a850ba70c02975d5cdf609b3
Shiroy <awacheux.utc@gmail.com>
2016-07-12 15:02:54 +0200

    Make gulp watch and live reload the client

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   entry: './client/src/index.ts',
   output: {
-    filename: 'bundle.js'
+    filename: 'ares.js'
   },
   resolve: {
     // Add `.ts` and `.tsx` as a resolvable extension.
@@ -11,6 +11,7 @@ module.exports = {
       'phaser': 'Phaser',
       'socket.io': 'io'
   },
+  devtool: "source-map",
   module: {
     loaders: [
       // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
