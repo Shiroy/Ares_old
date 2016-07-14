afc6db0aee91bc2c1ad2990d489ecd215d8761f1
shiroy <awacheux.utc@gmail.com>
2016-07-14 14:04:01 +0200

    Add an observer system

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,7 @@ import serve_static = require('serve-static');
 import http = require('http');
 import morgan = require('morgan');
 import socket_io = require("socket.io");
+import {Observer} from './util/signal';
 
 let app = express();
 let server = http.createServer(app);
@@ -14,6 +15,7 @@ app.use(morgan('dev'));
 
 io.on('connection', () => {
     console.log("Nouvelle connexion");
+    Observer.emit("new-connection");
 })
 
 app.use(serve_static(__dirname + '/public', {
