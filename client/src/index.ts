a15aca1636f8b0d7dae189f6c3549beaecdf693f
Théophile DANCOISNE <dancoisne.theophile@gmail.com>
2016-07-18 00:24:08 +0200

    set skill bar

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,7 +1,11 @@
 //Write the client code here
 import {Game} from "./ares";
+import {init_buttons} from "./ui"
+
 import io = require("socket.io");
+
 
 io();
 
 let ares : Game = new Game();
+init_buttons(ares);
