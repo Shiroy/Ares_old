a56c75ab5e89321f5dd34275dd6e595fbe637a6f
Théophile DANCOISNE <dancoisne.theophile@gmail.com>
2016-07-18 15:39:00 +0200

    spell template complete

--- client/src/spell.ts
+++ client/src/spell.ts
@@ -3,58 +3,71 @@ import {Player} from './player';
 
 
 export abstract class spell{
-    private _castTime: number;
-    private _coolDownTime: number;
-    private _energyCost: number;
-    private _lifeCost: number;
-    constructor(castTime_: number = 0, coolDownTime_: number = 0, energyCost_: number = 0, lifeCost_: number = 0){
-        if(castTime_ < 0 || coolDownTime_ < 0 || energyCost_ < 0 || lifeCost_ < 0){
-            throw "SpellTemplate initialization error: incorrect parameters";
-        }
-        this._castTime = castTime_;
-        this._coolDownTime = coolDownTime_;
-        this._energyCost = energyCost_;
-        this._lifeCost = lifeCost_;
+  private _castTime: number;
+  private _coolDownTime: number;
+  private _energyCost: number;
+  private _lifeCost: number;
+  protected _last_use: number;
+  constructor(castTime_: number = 0, coolDownTime_: number = 0, energyCost_: number = 0, lifeCost_: number = 0){
+    if(castTime_ < 0 || coolDownTime_ < 0 || energyCost_ < 0 || lifeCost_ < 0){
+      throw "SpellTemplate initialization error: incorrect parameters";
     }
-    abstract apply(game: Phaser.Game, player: Player);
-    set castTime(castTime_: number){
-        if (castTime_ < 0) throw "SpellTemplate: incorrect castTime";
-        this._castTime = castTime_;
+    this._castTime = castTime_;
+    this._coolDownTime = coolDownTime_;
+    this._energyCost = energyCost_;
+    this._lifeCost = lifeCost_;
+
+    this._last_use = -coolDownTime_;
+  }
+  apply(player: Player){
+    if(player.game.physics.arcade.distanceBetween(player, player.target) < player.scope){
+      if(player.energy >= this.energyCost && player.game.time.now - this._last_use > this._coolDownTime){
+        player.energy -= this.energyCost;
+        let timer = player.game.time.create();
+        timer.add(this.castTime, this._effects.bind(this, player));
+        timer.start();
+      }
     }
-    set coolDownTime(coolDownTime_: number){
-        if (coolDownTime_ < 0) throw "SpellTemplate: incorrect coolDownTime";
-        this._coolDownTime = coolDownTime_;
-    }
-    set energyCost(energyCost_: number){
-        if (energyCost_ < 0) throw "SpellTemplate: incorrect castTime";
-        this._energyCost = energyCost_;
-    }
-    set lifeCost(lifeCost_: number){
-        if (lifeCost_ < 0) throw "SpellTemplate: incorrect lifeCost";
-        this._lifeCost = lifeCost_;
-    }
-    get castTime(){
-        return this._castTime;
-    }
-    get coolDownTime(){
-        return this._coolDownTime;
-    }
-    get energyCost(){
-        return this._energyCost;
-    }
-    get lifeCost(){
-        return this._lifeCost;
-    }
+  }
+  protected abstract _effects(player: Player);
+
+  set castTime(castTime_: number){
+    if (castTime_ < 0) throw "SpellTemplate: incorrect castTime";
+    this._castTime = castTime_;
+  }
+  set coolDownTime(coolDownTime_: number){
+    if (coolDownTime_ < 0) throw "SpellTemplate: incorrect coolDownTime";
+    this._coolDownTime = coolDownTime_;
+  }
+  set energyCost(energyCost_: number){
+    if (energyCost_ < 0) throw "SpellTemplate: incorrect castTime";
+    this._energyCost = energyCost_;
+  }
+  set lifeCost(lifeCost_: number){
+    if (lifeCost_ < 0) throw "SpellTemplate: incorrect lifeCost";
+    this._lifeCost = lifeCost_;
+  }
+  get castTime(){
+    return this._castTime;
+  }
+  get coolDownTime(){
+    return this._coolDownTime;
+  }
+  get energyCost(){
+    return this._energyCost;
+  }
+  get lifeCost(){
+    return this._lifeCost;
+  }
 }
 
 export class spell_attack extends spell{
   constructor(){
     super(800, 5*Phaser.Timer.SECOND, 5);
   }
-  apply(game: Phaser.Game, player: Player){
-    if(game.physics.arcade.distanceBetween(player, player.target) < player.scope){
-      player.target.damage(15);
-    }
+  _effects(player: Player){
+    player.target.damage(15);
+    this._last_use = player.game.time.now;
   }
 }
 
