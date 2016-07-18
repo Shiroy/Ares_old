cef1a02c1f3041f5dd822e69e53ba385921fcb7f
Théophile DANCOISNE <dancoisne.theophile@gmail.com>
2016-07-18 15:39:00 +0200

    spell template complete

--- client/src/player.ts
+++ client/src/player.ts
@@ -8,12 +8,23 @@ export class Player extends Entity{
   private _scope: number;
   private _maxSpeed: number;
 
+  private _energy: number;
+  private _maxEnergy: number;
+
   private _target: Phaser.Sprite;
   private _following_target: boolean;
 
   private _graphic_scope: Phaser.Graphics;
 
-  constructor(game: Phaser.Game, x: number, y: number, key: string | Phaser.RenderTexture | Phaser.BitmapData | PIXI.Texture, frame: string | number, spells: Array<spell>, scope: number = 230, maxHealth: number = 100, maxSpeed: number = 300){
+  constructor(game: Phaser.Game,
+    x: number, y: number, key: string | Phaser.RenderTexture | Phaser.BitmapData | PIXI.Texture,
+    frame: string | number,
+    spells: Array<spell>,
+    scope: number = 230,
+    maxHealth: number = 500,
+    maxSpeed: number = 300,
+    maxEnergy: number = 60){
+
     // call to the Phaser.Sprite constructor
     super(game, x, y, key, frame, maxHealth);
     if(spells.length > 8) new ares_exception('Player', 'constructor', 'too many spells');
@@ -21,6 +32,9 @@ export class Player extends Entity{
 
     this._scope = scope;
     this._maxSpeed = maxSpeed;
+
+    this._maxEnergy = maxEnergy;
+    this._energy = maxEnergy;
 
     this._target = this;
     this._following_target = false;
@@ -40,6 +54,18 @@ export class Player extends Entity{
   set maxSpeed(maxSpeed: number){
     this._maxSpeed = maxSpeed;
   }
+  get maxEnergy(){
+    return this._maxEnergy;
+  }
+  set maxEnergy(maxEnergy: number){
+    this._maxEnergy = maxEnergy;
+  }
+  get energy(){
+    return this._energy;
+  }
+  set energy(energy: number){
+    this._energy = energy;
+  }
   get target(){
     return this._target;
   }
@@ -55,7 +81,7 @@ export class Player extends Entity{
 
   apply_spell(i: number){
     if(!this._spells[i]) throw new ares_exception('Player', 'apply_spell', 'the spell ' + i + ' doesn\'t exists');
-    this._spells[i].apply(this.game, this);
+    this._spells[i].apply(this);
   }
 
   debug_scope(): Phaser.Graphics{
